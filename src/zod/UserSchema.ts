1f4d03b28208463543557b27d943cb1e4f1fdc5d
Yash Anand <yash.anand167@gmail.com>
2025-06-16 14:42:53 +0530

    feat: Add height and weight fields to userHealth schema

--- src/zod/UserSchema.ts
+++ src/zod/UserSchema.ts
@@ -1,6 +1,8 @@
 import z from "zod";
 
 export const userHealth = z.object({
+  height: z.number(),
+  weight: z.number(),
   currentImage: z.string(),
   Gender: z.enum(["Male", "Female"]),
   BodyType: z.enum([
