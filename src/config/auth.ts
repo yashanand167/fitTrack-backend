11584a4735c871c11af207d92af74a04ca410c50
Yash Anand <yash.anand167@gmail.com>
2025-06-16 15:00:44 +0530

    refactor: Update MongoDB client initialization to use env schema for configuration

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,8 +1,9 @@
 import { betterAuth } from "better-auth";
+import { env } from "../zod/env.schema";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.MONGODB_URI as string);
+const client = new MongoClient(env.MONGODB_URI);
 const db = client.db();
 
 const auth = betterAuth({
